3073057b9fb3a96550b49274e8a4d2a4799a1507
rishitmaurya <rishitmaurya11@gmail.com>
2025-05-29 19:46:35 +0530

    added a menu bar

--- gui/app.py
+++ gui/app.py
@@ -1,6 +1,7 @@
 # app.py
-from PyQt5.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QWidget, QLabel
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QVBoxLayout, QWidget, QLabel, QAction, QMenuBar, QMessageBox
 from gui.camera_widget import CameraWidget
+from PyQt5.QtCore import Qt
 
 class MainApp(QMainWindow):
     def __init__(self):
@@ -24,3 +25,29 @@ class MainApp(QMainWindow):
         container = QWidget()
         container.setLayout(layout)
         self.setCentralWidget(container)
+        
+        self.create_menu()
+
+    def create_menu(self):
+        menubar = self.menuBar()
+
+        # File menu
+        file_menu = menubar.addMenu("File")
+
+        exit_action = QAction("Exit", self)
+        exit_action.triggered.connect(self.close)
+        file_menu.addAction(exit_action)
+
+        # Help menu
+        help_menu = menubar.addMenu("Help")
+
+        about_action = QAction("About", self)
+        about_action.triggered.connect(self.show_about_dialog)
+        help_menu.addAction(about_action)
+
+    def show_about_dialog(self):
+        QMessageBox.information(
+            self,
+            "About",
+            "Human Trespass Detection System\nDeveloped using PyQt5, OpenCV, and YOLOv8."
+        )
